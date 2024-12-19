155d89585346019347b0397a66ff48e4c8a168c3
mykola <prpl.ha@gmail.com>
2024-12-19 17:57:48 +0100

    fix: leaderboard top margin for mobile view

--- app/(home)/dashboard/layout.tsx
+++ app/(home)/dashboard/layout.tsx
@@ -14,7 +14,7 @@ export default function DashboardLayout({
       <section className="fixed top-0 left-0 right-0 md:static border-b border-default-100 w-full h-[12%] md:h-[9%] bg-background z-10">
         {children}
       </section>
-      <div className="md:flex w-full h-[91%] mt-[12%] md:mt-0">
+      <div className="md:flex w-full h-[91%] mt-[25%] md:mt-0">
         <section className="md:w-1/2 border-r border-b md:border-b-0 border-default-100">
           {leaderboard}
         </section>
